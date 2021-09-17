6114edc33f559c1a8dde9a4b07d74d19a81d7e6f
hermo <javierhmorant@gmail.com>
2021-09-17 18:14:10 +0200

    version mejorada

--- prjavi.java
+++ prjavi.java
@@ -1,8 +1,9 @@
 public class prjavi{
 	public static void main(String[] args){
-		String x;
-		int resultado = 0;
+		double x=5;
+		double resultado = 0;
 		
 		resultado = Math.pow(x,2);
+		System.out.println(resultado);
 	}
 }
